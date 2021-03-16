f3b8ee1d19576cbccf11288343a28c232f404a57
Wagsnerlund <chelseawagner17@gmail.com>
2021-03-16 23:30:27 +0000

    added edit button with functionality

--- express_server.js
+++ express_server.js
@@ -1,19 +1,19 @@
 const { request, response } = require('express');
 const express = require('express');
-const app = express();
 const bodyParser = require("body-parser");
+const app = express();
 const PORT = 8080;
 
 function generateRandomString() {
   return Math.random().toString(36).slice(2, 8);
 };
 
-app.set('view engine', 'ejs');
-
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
 
@@ -35,7 +35,9 @@ app.get('/urls/new', (request, response) => {
 });
 
 app.get('/urls/:shortURL', (request, response) => {
-  const templateVars = { shortURL: request.params.shortURL, longURL: urlDatabase[request.params.shortURL]};
+  const shortURL = request.params.shortURL;
+  const longURL = urlDatabase[request.params.shortURL];
+  const templateVars = { shortURL, longURL };
   response.render('urls_show', templateVars);
 });
 
@@ -49,17 +51,31 @@ app.get('/hello', (request, response) => {
 });
 
 app.post('/urls', (request, response) => {
-  let short = generateRandomString();
-  urlDatabase[short] = request.body.longURL;
-  response.redirect(`/urls/${short}`);
+  let shortURL = generateRandomString();
+  urlDatabase[shortURL] = request.body.longURL;
+  response.redirect(`/urls/${shortURL}`);
 });
 
 app.post('/urls/:shortURL/delete', (request, response) => {
   const url = request.params.shortURL
   delete urlDatabase[url];
   response.redirect('/urls');
-})
+});
+
+app.post('/urls/:shortURL/edit', (request, response) => {
+  urlDatabase[request.params.shortURL] = request.body.longURL;
+  response.redirect(`/urls`);
+});
+
+app.post('/urls/:id', (request, response) => {
+  const url = request.params.id
+  response.redirect(`/urls/${url}`);
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
 });
+
+// This activity will implement the Update operation using a POST request, which will allow us to edit existing shortened URLs in our application.
+
+// This portion of the assignment requires changes on the client and the server. Once the user submits an Update request, it should modify the corresponding longURL, and then redirect the client back to "/urls".
